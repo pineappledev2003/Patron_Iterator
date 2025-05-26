ec9ecfe91e0d1a57d6c985d4431ce51c01041028
PiñaOficial <89712014+Flying-Bison1018@users.noreply.github.com>
2025-05-26 14:49:38 -0500

    second_commit

--- iterator_flutter_dart/lib/patron_iterator/concreteIterator/user_list_iterator.dart
+++ iterator_flutter_dart/lib/patron_iterator/concreteIterator/user_list_iterator.dart
@@ -1,17 +1,41 @@
+// ignore_for_file: slash_for_doc_comments
+
 import 'package:iterator_flutter_dart/models/user.dart';
 import 'package:iterator_flutter_dart/patron_iterator/iterator/interfaz_iterator.dart';
 
 class UserListIterator implements InterfazIterator<User> {
-  final List<User> _users;
-  int _position = 0;
-  User? _currentUser;
+  final List<User> _users; //Lista original de usuarios
+  int _position = 0; // Posicion actual dentro de la lista
+  User? _currentUser; // Guarda el ultimo usuario que se devolvio con next()
 
   UserListIterator({required List<User> users}) : _users = users;
+
+  /**
+   *  ? Verifica si todavía quedan elementos por recorrer.
+   *  ? Si _position es menor que el largo de la lista, retorna true.
+   *  ? Cuando _position alcanza el final de la lista, retorna false.
+   *  
+   *  ! Ejemplo
+   *  ? Lista de 3 elementos (length = 3), si _position = 0, hay más → true.
+   *  ? i _position = 3, ya terminó → false.
+   */
 
   @override
   bool hasNext() {
     return _position < _users.length;
   }
+
+  /**
+   * ? Si ya no hay más elementos (!hasNext()), lanza una excepción.
+   * ? Si sí hay:
+   * ? Guarda el usuario actual en _currentUser.
+   * ? Incrementa _position para apuntar al siguiente.
+   * ? Retorna el usuario actual.
+   * 
+   * ! Ejemplo
+   * ? _position = 0, devuelve _users[0], luego _position = 1.
+   * ? _position = 1, devuelve _users[1], luego _position = 2.
+   */
 
   @override
   User next() {
@@ -23,11 +47,22 @@ class UserListIterator implements InterfazIterator<User> {
     }
   }
 
+  /**
+   * ? Reinicia el iterador:
+   * ? Pone _position en 0 (vuelve al inicio).
+   * ? Limpia _currentUser.
+   */
+
   @override
   void reset() {
     _position = 0;
     _currentUser = null;
   }
+
+  /**
+   * ? Devuelve el usuario actual, es decir, el último que devolvió next().
+   * ? Si aún no se llamó a next(), lanza una excepción (porque no hay "usuario actual").
+   */
 
   @override
   User current() {
--- iterator_flutter_dart/lib/patron_iterator/concreteIterator/user_map_iterator.dart
+++ iterator_flutter_dart/lib/patron_iterator/concreteIterator/user_map_iterator.dart
@@ -1,29 +1,45 @@
+// ignore_for_file: slash_for_doc_comments
+
 import 'package:iterator_flutter_dart/models/user.dart';
 import 'package:iterator_flutter_dart/patron_iterator/iterator/interfaz_iterator.dart';
 
 class UserMapIterator implements InterfazIterator<User> {
   final Map<String, List<User>> _usersByRole;
   late List<String> _roles; // Claves del mapa
-  int _roleIndex = 0; // en qué **rol (clave)** estamos.
-  int _userIndex = 0; // en qué **usuario dentro de la lista** de ese rol estamos.
-  User? _currentUser;
+  int _roleIndex = 0; // en qué *rol (clave)* estamos.
+  int _userIndex = 0; // en qué *usuario dentro de la lista* de ese rol estamos.
+  User? _currentUser; // Usuario actual, actualizado con next()
 
   UserMapIterator({required Map<String, List<User>> users})
     : _usersByRole = users {
-    _roles = _usersByRole.keys.toList();
+    _roles = _usersByRole.keys.toList(); // Se guardan las claves (roles)
+
+    /**
+     * {
+     *    "Admin": [User1, User2],
+     *     "User": [User3, User4, User5]
+     *  }
+     * 
+     *  ? _roles = ["Admin", "User"];
+     */
   }
+
+  /**
+   * Este método verifica si aún hay usuarios por recorrer, revisando cada rol en orden
+   * Si ya terminó con una lista, pasa al siguiente rol automáticamente.
+   */
 
   @override
   bool hasNext() {
     while (_roleIndex < _roles.length) {
-      final role = _roles[_roleIndex];
-      final users = _usersByRole[role];
+      final role = _roles[_roleIndex]; // Rol actual
+      final users = _usersByRole[role]; // Lista de usuarios de ese rol
 
       if (_userIndex < users!.length) {
-        return true;
+        return true; // Si todavia hay usuarios en esta lista
       } else {
-        _roleIndex++;
-        _userIndex = 0;
+        _roleIndex++; // Pasamos al siguiente rol
+        _userIndex = 0; // Reiniciamos el indice de usuario
       }
     }
     return false;
@@ -31,13 +47,15 @@ class UserMapIterator implements InterfazIterator<User> {
 
   @override
   User next() {
+    // Verificamos si aún quedan usuarios por recorrer
     if (!hasNext()) {
-      throw Exception("No hay más usuarios");
+      throw Exception("No hay más usuarios"); // Si no hay más, lanzamos excepción
     } else {
-      final role = _roles[_roleIndex];
-      final users = _usersByRole[role]!;
-      _currentUser = users[_userIndex++];
-      return _currentUser!;
+      
+      final role = _roles[_roleIndex]; // Obtenemos el rol actual usando el índice de rol
+      final users = _usersByRole[role]!; // Obtenemos la lista de usuarios correspondiente a ese rol
+      _currentUser = users[_userIndex++]; // Asignamos el usuario actual y avanzamos al siguiente índice de usuario
+      return _currentUser!; // Retornamos el usuario actual
     }
   }
 
